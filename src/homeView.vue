60d9cb0cf8d119fd70faeedaa753f2a0e7610729
jomarbolasco <jomar.study@gmail.com>
2024-11-18 12:44:56 +0800

    feat: added webpagelayout using slot

---
+++ src/homeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import appLayout from './components/layout/appLayout.vue'
+</script>
+
+<template>
+  <appLayout>
+    <template #content>
+      <h1>application picture here</h1>
+    </template>
+  </appLayout>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import homeView from '@/views/auth/homeView.vue'
+import homeView from '@/homeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
