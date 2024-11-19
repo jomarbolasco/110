24ed26efa1675bdc7055a0b8c781d5d134ea109a
jomarbolasco <jomar.study@gmail.com>
2024-11-19 17:27:04 +0800

    feat: login form added validations

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,3 +1,29 @@
+<script setup>
+import { requiredValidator, emailValidator } from '@/components/util/validators'
+import { ref } from 'vue'
+
+const refVForm = ref()
+
+const formDataDefault = {
+  email: '',
+  password: '',
+}
+
+const formData = ref({
+  ...formDataDefault,
+})
+
+const onLogin = () => {
+  alert(formData.value.email)
+}
+
+const onFormSubmit = () => {
+  refVForm.value.validate().then(({ valid }) => {
+    if (valid) onLogin()
+  })
+}
+</script>
+
 <script>
 export default {
   data: () => ({
@@ -9,18 +35,21 @@ export default {
 <template>
   <div class="text-subtitle-1 text-medium-emphasis">Account</div>
 
-  <v-text-field
-    density="compact"
-    placeholder="Email address"
-    prepend-inner-icon="mdi-email-outline"
-    variant="outlined"
-  ></v-text-field>
+  <v-form ref="refVForm" @submit.prevent="onFormSubmit">
+    <v-text-field
+      v-model="formData.email"
+      :rules="[requiredValidator, emailValidator]"
+      density="compact"
+      placeholder="Email address"
+      prepend-inner-icon="mdi-email-outline"
+      variant="outlined"
+    ></v-text-field>
 
-  <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-    Password
+    <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+      Password
 
-    <!-- change password button -->
-    <!-- <a
+      <!-- change password button -->
+      <!-- <a
                       class="text-caption text-decoration-none text-blue"
                       href="#"
                       rel="noopener noreferrer"
@@ -28,19 +57,21 @@ export default {
                     >
                       Forgot login password?</a
                     > -->
-  </div>
+    </div>
 
-  <v-text-field
-    :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-    :type="visible ? 'text' : 'password'"
-    density="compact"
-    placeholder="Enter your password"
-    prepend-inner-icon="mdi-lock-outline"
-    variant="outlined"
-    @click:append-inner="visible = !visible"
-  ></v-text-field>
+    <v-text-field
+      v-model="formData.password"
+      :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+      :type="visible ? 'text' : 'password'"
+      :rules="[requiredValidator]"
+      density="compact"
+      placeholder="Enter your password"
+      prepend-inner-icon="mdi-lock-outline"
+      variant="outlined"
+      @click:append-inner="visible = !visible"
+    ></v-text-field>
 
-  <!-- <v-card class="mb-12" color="surface-variant" variant="tonal">
+    <!-- <v-card class="mb-12" color="surface-variant" variant="tonal">
                     <v-card-text class="text-medium-emphasis text-caption">
                       Warning: After 3 consecutive failed login attempts, you account will be
                       temporarily locked for three hours. If you must login now, you can also click
@@ -48,11 +79,14 @@ export default {
                     </v-card-text>
                   </v-card> -->
 
-  <v-btn class="mb-8" color="blue" size="large" variant="tonal" block> Log In </v-btn>
+    <v-btn class="mb-8" color="blue" size="large" variant="tonal" block type="submit">
+      Log In
+    </v-btn>
 
-  <v-card-text class="text-center">
-    <RouterLink to="/register" class="text-blue text-decoration-none" rel="noopener noreferrer">
-      Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
-    </RouterLink>
-  </v-card-text>
+    <v-card-text class="text-center">
+      <RouterLink to="/register" class="text-blue text-decoration-none" rel="noopener noreferrer">
+        Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+      </RouterLink>
+    </v-card-text>
+  </v-form>
 </template>
