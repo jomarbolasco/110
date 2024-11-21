99d7cc3a5f007c124ce81e8fbe7b57fb7b4f29c0
Ryan Jose Silab <razorryan20@gmail.com>
2024-11-21 13:10:58 +0800

    feat: fx sign in button

--- src/components/util/supabase.js
+++ src/components/util/supabase.js
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(import.meta.VITE_SUPABASE_URL, import.meta.VITE_SUPABASE_ANON_KEY)
+export const supabase = createClient(
+  import.meta.VITE_SUPABASE_URL,
+  import.meta.VITE_SUPABASE_ANON_KEY,
+)
