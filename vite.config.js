4be49d9911c4b4c0d6495fbde12d194d4c91a0b0
joamrbolasco <jomar.student@gmail.com>
2024-11-21 09:46:17 -0800

    fix: fixing deployment issue = updating vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': {},
+  },
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
