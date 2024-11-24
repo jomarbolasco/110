349f00db25051de06f132c61457fbb230bcecad4
joamrbolasco <jomar.student@gmail.com>
2024-11-24 15:00:17 +0800

    partilay branch for cleaning.. feat: added appview

---
+++ src/components/layout/appLayout.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref(localStorage.getItem('theme') ?? 'dark')
+</script>
+
+<template>
+  <v-responsive>
+    <v-app :theme="theme">
+      <v-app-bar class="px-3">
+        <slot name="navbar"></slot>
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <slot name="content"></slot>
+        </v-container>
+      </v-main>
+
+      <v-footer class="font-weight-bold" border app>
+        <div class="w-100 text-center">Copyright © 2024 - HospiAPP | All Rights Reserved</div>
+      </v-footer>
+    </v-app>
+  </v-responsive>
+</template>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,80 +1,72 @@
 <script setup>
 import RegisterForm from '@/components/auth/RegisterForm.vue'
 import { ref } from 'vue'
+import appLayout from '@/components/layout/appLayout.vue'
 
 const theme = ref('dark')
 </script>
 
 <template>
-  <v-responsive>
-    <v-app :theme="theme">
-      <v-app-bar class="px-3">
-        <v-btn append-icon="mdi-home" prepend-icon="mdi-check-circle">
-          <template v-slot:prepend>
-            <!-- <v-icon color="success"></v-icon> -->
-          </template>
-          <RouterLink style="text-decoration: none" to="/">HOME</RouterLink>
-          <template v-slot:append>
-            <v-icon color="warning"></v-icon>
-          </template>
-        </v-btn>
-        <v-spacer></v-spacer>
+  <appLayout>
+    <template #navbar>
+      <v-btn append-icon="mdi-home" prepend-icon="mdi-check-circle">
+        <template v-slot:prepend> </template>
+        <RouterLink style="text-decoration: none" to="/">HOME</RouterLink>
+        <template v-slot:append>
+          <v-icon color="warning"></v-icon>
+        </template>
+      </v-btn>
+      <v-spacer></v-spacer>
 
-        <v-btn append-icon="mdi-account" prepend-icon="mdi-check-circle">
-          <template v-slot:prepend>
-            <!-- <v-icon color="success"></v-icon> -->
-          </template>
-          <RouterLink style="text-decoration: none" to="/login">Login</RouterLink>
-          <template v-slot:append>
-            <v-icon color="warning"></v-icon>
-          </template>
-        </v-btn>
+      <v-btn append-icon="mdi-account" prepend-icon="mdi-check-circle">
+        <template v-slot:prepend> </template>
+        <RouterLink style="text-decoration: none" to="/login">Login</RouterLink>
+        <template v-slot:append>
+          <v-icon color="warning"></v-icon>
+        </template>
+      </v-btn>
 
-        <v-btn active append-icon="mdi-account-plus" prepend-icon="mdi-check-circle">
-          <template v-slot:prepend>
-            <!-- <v-icon color="success"></v-icon> -->
-          </template>
-          <RouterLink style="text-decoration: none" to="/register">Sign-up</RouterLink>
-          <template v-slot:append>
-            <v-icon color="warning"></v-icon>
-          </template>
-        </v-btn>
-      </v-app-bar>
+      <v-btn active append-icon="mdi-account-plus" prepend-icon="mdi-check-circle">
+        <template v-slot:prepend> </template>
+        <RouterLink style="text-decoration: none" to="/register">Sign-up</RouterLink>
+        <template v-slot:append>
+          <v-icon color="warning"></v-icon>
+        </template>
+      </v-btn>
+    </template>
 
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="5">
-              <div>
-                <v-img
-                  class="mx-auto my-6"
-                  max-width="228"
-                  src="https://onhlawoqasmyceacldah.supabase.co/storage/v1/object/public/images/logoname.png"
-                ></v-img>
-                <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
-                  <v-divider class="mb-5" />
+    <template #content
+      ><v-container>
+        <v-row>
+          <v-col cols="12" md="5">
+            <div>
+              <v-img
+                class="mx-auto my-6"
+                max-width="228"
+                src="https://onhlawoqasmyceacldah.supabase.co/storage/v1/object/public/images/logoname.png"
+              ></v-img>
+              <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
+                <v-divider class="mb-5" />
 
-                  <RegisterForm />
+                <RegisterForm />
 
-                  <v-divider class="my-3" />
-                  <v-card-text class="text-center">
-                    <p>
-                      Already have an account?
-                      <RouterLink
-                        to="/login"
-                        class="text-blue text-decoration-none"
-                        rel="noopener noreferrer"
-                      >
-                        Login <v-icon icon="mdi-login"></v-icon>
-                      </RouterLink>
-                    </p>
-                  </v-card-text>
-                </v-card>
-              </div>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </v-app>
-  </v-responsive>
+                <v-divider class="my-3" />
+                <v-card-text class="text-center">
+                  <p>
+                    Already have an account?
+                    <RouterLink
+                      to="/login"
+                      class="text-blue text-decoration-none"
+                      rel="noopener noreferrer"
+                    >
+                      Login <v-icon icon="mdi-login"></v-icon>
+                    </RouterLink>
+                  </p>
+                </v-card-text>
+              </v-card>
+            </div>
+          </v-col>
+        </v-row> </v-container
+    ></template>
+  </appLayout>
 </template>
