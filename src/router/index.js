c578cf9a429b5c55af1d09f8dc8317bc4363625b
joamrbolasco <jomar.student@gmail.com>
2024-11-25 09:07:17 +0800

    feat: removing ui-components,vuetifycomponents.(complicated)

--- src/router/index.js
+++ src/router/index.js
@@ -36,34 +36,29 @@ const router = createRouter({
           component: () => import('@/views/dashboard/DashBoard.vue'),
         },
         // {
-        //   name: "Alerts",
-        //   path: "ui-components/alert",
-        //   component: () =>
-        //     import("@/views/ui-components/Alerts.vue"),
+        //   name: 'Alerts',
+        //   path: 'ui-components/alert',
+        //   component: () => import('@/views/ui-components/Alerts.vue'),
         // },
         // {
-        //   name: "Buttons",
-        //   path: "ui-components/buttons",
-        //   component: () =>
-        //     import("@/views/ui-components/Buttons.vue"),
+        //   name: 'Buttons',
+        //   path: 'ui-components/buttons',
+        //   component: () => import('@/views/ui-components/Buttons.vue'),
         // },
         // {
-        //   name: "Cards",
-        //   path: "ui-components/cards",
-        //   component: () =>
-        //     import("@/views/ui-components/Cards.vue"),
+        //   name: 'Cards',
+        //   path: 'ui-components/cards',
+        //   component: () => import('@/views/ui-components/Cards.vue'),
         // },
         // {
-        //   name: "Menus",
-        //   path: "ui-components/menus",
-        //   component: () =>
-        //     import("@/views/ui-components/Menus.vue"),
+        //   name: 'Menus',
+        //   path: 'ui-components/menus',
+        //   component: () => import('@/views/ui-components/Menus.vue'),
         // },
         // {
-        //   name: "Tables",
-        //   path: "ui-components/tables",
-        //   component: () =>
-        //     import("@/views/ui-components/Tables.vue"),
+        //   name: 'Tables',
+        //   path: 'ui-components/tables',
+        //   component: () => import('@/views/ui-components/Tables.vue'),
         // },
         {
           path: '/:pathMatch(.*)*',
