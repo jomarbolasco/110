ed2f178df9383e29e30d0d1a233f87d7e5eb235e
joamrbolasco <jomar.student@gmail.com>
2024-11-26 18:20:43 +0800

    fix: clearing appointment page

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import { useUserStore } from '@/stores/user'
 import homeView from '@/homeView.vue'
 import loginView from '@/views/auth/loginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
-import Appointments from '@/views/ui-components/Appointments.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
