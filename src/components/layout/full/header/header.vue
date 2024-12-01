3f7be9d054a0a17a7cf3acce74fc8e6d766fe674
joamrbolasco <jomar.student@gmail.com>
2024-12-01 23:50:14 +0800

    fix: replacing user.js to userStore.js

--- src/components/layout/full/header/header.vue
+++ src/components/layout/full/header/header.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { useUserStore } from '@/stores/user'
+import { useUserStore } from '@/stores/userStore'
 import { useRouter } from 'vue-router'
 import { supabase } from '@/components/util/supabase'
 
