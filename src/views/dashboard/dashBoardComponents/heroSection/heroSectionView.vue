09ac9742c4c3d27aaa592fb42ca7f655f3abdbc7
jomarbolasco <jomar.study@gmail.com>
2024-12-27 12:51:24 +0800

    refactor: remove unused script and sparkline component from hero section for cleaner code

--- src/views/dashboard/dashBoardComponents/heroSection/heroSectionView.vue
+++ src/views/dashboard/dashBoardComponents/heroSection/heroSectionView.vue
@@ -1,34 +1,6 @@
-<script>
-const gradients = [
-  ['#222'],
-  ['#42b3f4'],
-  ['red', 'orange', 'yellow'],
-  ['purple', 'violet'],
-  ['#00c6ff', '#F0F', '#FF0'],
-  ['#f72047', '#ffd200', '#1feaea'],
-]
-
-export default {
-  data: () => ({
-    width: 2,
-    radius: 10,
-    padding: 8,
-    lineCap: 'round',
-    gradient: gradients[5],
-    value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
-    gradientDirection: 'top',
-    gradients,
-    fill: false,
-    type: 'trend',
-    autoLineWidth: false,
-  }),
-}
-</script>
+<script setup></script>
 
 <template>
-  <!-- ------------------------------------ -->
-  <!-- html -->
-  <!-- ------------------------------------ -->
   <v-card>
     <v-card-text class="pa-7">
       <div class="d-sm-flex align-center mb-5">
@@ -37,20 +9,6 @@ export default {
         </div>
       </div>
       <v-spacer></v-spacer>
-      <v-sparkline
-        :auto-line-width="autoLineWidth"
-        :fill="fill"
-        :gradient="gradient"
-        :gradient-direction="gradientDirection"
-        :line-width="width"
-        :model-value="value"
-        :padding="padding"
-        :smooth="radius || false"
-        :stroke-linecap="lineCap"
-        :type="type"
-        :loop="true"
-        auto-draw
-      ></v-sparkline>
     </v-card-text>
   </v-card>
 </template>
